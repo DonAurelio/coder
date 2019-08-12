646ec85975256ef2a5e2b7a08075f0d92d0fa182
Aurelio Vivas <aa.vivas@uniandes.edu.co>
2019-08-12 09:56:40 -0500

    initial database data for kubernetes and docker-compose

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -6,9 +6,6 @@
 
 ./manage.py collectstatic --noinput
 
-# Initial db data
-./manage.py loaddata example-db.json
-
 gunicorn coder.wsgi --timeout 600 -b 0.0.0.0:8001
 
 # ./manage.py runserver 0.0.0.0:80
