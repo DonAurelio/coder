daf55c8e0c222f1c2a08de9cdb97644b373e96e2
Aurelio Vivas <aa.vivas@uniandes.edu.co>
2019-08-12 08:40:59 -0500

    deployment with kubernetes

--- client/src/app/services/config.service.ts
+++ client/src/app/services/config.service.ts
@@ -13,10 +13,14 @@ export class ConfigService {
   templates_service: any;
 
   constructor() { 
-    this.pragcc_service = {'url': 'http://nginx:8000'};
-    this.file_service = {'url': 'http://nginx:8000'};
-    this.project_service = {'url': 'http://nginx:8000'};
-    this.templates_service = {'url': 'http://nginx:8000'};
+    // A real deployment requiered the services be available 
+    // over the internet by a DNS name.
+    
+    this.pragcc_service = {'url': 'http://coder_server:8000'};
+    this.file_service = {'url': 'http://coder_server:8000'};
+    this.project_service = {'url': 'http://coder_server:8000'};
+    this.templates_service = {'url': 'http://coder_server:8000'};
+
   }
 
 }
